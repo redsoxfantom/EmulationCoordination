11702f1ccf24d8ebabbe63d3c44f1b5eb26b1f5b
redsoxfantom <redsoxfantom@gmail.com>
2016-12-26 14:41:53 -0500

    Moved Scrapers to its own dll

---
+++ EmulationCoordination.Scrapers/Scrapers/IScraper.cs
@@ -0,0 +1,16 @@
+﻿using EmulationCoordination.Roms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmulationCoordination.Scrapers.Scrapers
+{
+    public interface IScraper
+    {
+        String FriendlyName { get; }
+        List<RomData> Search(RomData dataToSearchFor);
+        RomData GetAllData(RomData dataToFillOut);
+    }
+}
