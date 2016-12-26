358d5e3304fabc71181f2fdf3801d7d305abfa17
redsoxfantom <redsoxfantom@gmail.com>
2016-12-26 15:13:15 -0500

    TreeView now updates with list of emulators

--- EmulationCoordination.Gui/Form1.cs
+++ EmulationCoordination.Gui/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using EmulationCoordination.Emulators;
+using EmulationCoordination.Emulators.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -14,17 +15,38 @@ namespace EmulationCoordination.Gui
     public partial class MainWindow : Form
     {
         private EmulatorManager emuMgr;
+        private List<IReadOnlyEmulator> emulators;
+        private List<IReadOnlyEmulator> installedEmulators;
+        private List<IReadOnlyEmulator> availableEmulators;
 
         public MainWindow()
         {
             InitializeComponent();
 
             emuMgr = EmulatorManager.Instance;
+            UpdateEmulators();
         }
 
         private void UpdateEmulators()
         {
-            emuMgr.GetAvailableEmulators();
+            emulators = emuMgr.GetAvailableEmulators();
+            installedEmulators = emulators.Where(f => f.Installed).ToList();
+            availableEmulators = emulators.Where(f => !f.Installed).ToList();
+
+            PopulateTreeNodes(EmulatorTreeView.Nodes["AvailableEmulators"].Nodes, availableEmulators);
+            PopulateTreeNodes(EmulatorTreeView.Nodes["InstalledEmulators"].Nodes, installedEmulators);
+        }
+
+        private void PopulateTreeNodes(TreeNodeCollection Nodes, List<IReadOnlyEmulator> emulators)
+        {
+            Nodes.Clear();
+            foreach(var emulator in emulators)
+            {
+                TreeNode newNode = new TreeNode();
+                newNode.Text = String.Format("{0} ({1})", emulator.EmulatorName, emulator.Version);
+                newNode.Tag = emulator;
+                Nodes.Add(newNode);
+            }
         }
     }
 }
