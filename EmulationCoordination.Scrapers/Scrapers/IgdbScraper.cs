f73a0fc071a61f8bd3c588904e4b60dbfc1fd7fc
redsoxfantom <redsoxfantom@gmail.com>
2016-12-27 14:23:24 -0500

    Igdb scraper retrieves images

--- EmulationCoordination.Scrapers/Scrapers/IgdbScraper.cs
+++ EmulationCoordination.Scrapers/Scrapers/IgdbScraper.cs
@@ -6,15 +6,18 @@ using System.Threading.Tasks;
 using EmulationCoordination.Roms;
 using EmulationCoordination.Scrapers.DataContracts.Igdb;
 using EmulationCoordination.Utilities;
+using System.Drawing;
 
 namespace EmulationCoordination.Scrapers.Scrapers
 {
     public class IgdbScraper : BaseScraper
     {
         private string searchRootUrl = "https://igdbcom-internet-game-database-v1.p.mashape.com/";
+        private string imageRootUrl = "https://images.igdb.com/igdb/image/upload/";
         private string searchFormat = "games/?fields=name%2Cid%2Crelease_dates&limit=10&offset=0&search={0}";
         private string getDataFormat = "games/{0}?fields=storyline%2Crating%2Cdevelopers%2Cpublishers%2Cscreenshots%2Cgame_modes&limit=10&offset=0&order=release_dates.date%3Adesc";
         private string getCompanyFormat = "companies/{0}?fields=id%2Cname";
+        private string getImageFormat = "t_{0}/{1}.jpg";
         private static string apiKey = "WAV35CMfrrmshHzymVUYTbw3Sz7sp1AjcD0jsnjooQzM79mbIk";
         Dictionary<String, String> headers = new Dictionary<string, string>()
         {
@@ -41,8 +44,52 @@ namespace EmulationCoordination.Scrapers.Scrapers
                 dataToFillOut.Rating = (float)resultData.rating / 100.0f;
                 dataToFillOut.Publisher = RetrieveCompanyInfo(resultData.publishers);
                 dataToFillOut.Developer = RetrieveCompanyInfo(resultData.developers);
+                RetrieveImages(dataToFillOut, resultData.screenshots);
             }
             return dataToFillOut;
+        }
+
+        private void RetrieveImages(RomData dataToFillOut,IgdbScreenshotData[] images)
+        {
+            if(images?.Length > 0)
+            {
+                // sorts the list into largest to smallest
+                var sortedImages = images.OrderBy(i => (i.width * i.height)).ToList();
+
+                // Largest image will be the background
+                string size = "screenshot_huge_2x";
+                string id = images.First().cloudinary_id;
+                dataToFillOut.Background = RequestImage(size, id);
+
+                // Smallest image will be the icon / logo
+                size = "logo_med_2x";
+                id = images.Last().cloudinary_id;
+                dataToFillOut.Logo = RequestImage(size, id);
+
+                // cover image will be the second smallest image
+                size = "cover_big_2x";
+                id = images.Count() > 1 ? images.ElementAt(images.Count()-2).cloudinary_id : images.First().cloudinary_id;
+                dataToFillOut.BoxArt = RequestImage(size, id);
+
+                // banner image will be the second largest image
+                size = "screenshot_big_2x";
+                id = images.Count() > 1 ? images.ElementAt(1).cloudinary_id : images.First().cloudinary_id;
+                dataToFillOut.Banner = RequestImage(size, id);
+            }
+            else
+            {
+                dataToFillOut.Background = Resource.DefaultBackground;
+                dataToFillOut.Banner = Resource.DefaultBanner;
+                dataToFillOut.BoxArt = Resource.DefaultBoxart;
+                dataToFillOut.Logo = Resource.DefaultIcon;
+            }
+        }
+
+        private Image RequestImage(string size, string id)
+        {
+            string finalUrl = string.Format(getImageFormat, size, id);
+            finalUrl = string.Format("{0}{1}", imageRootUrl, finalUrl);
+            return MakeImageRequest(finalUrl, headers);
         }
 
         private string RetrieveCompanyInfo(long[] ids)
