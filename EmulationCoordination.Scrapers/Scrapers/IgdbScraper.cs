2063974c925809d0b1681c25139799cb9cf7c273
redsoxfantom <redsoxfantom@gmail.com>
2016-12-27 13:56:01 -0500

    Igdb scraper can now extract publisher info

--- EmulationCoordination.Scrapers/Scrapers/IgdbScraper.cs
+++ EmulationCoordination.Scrapers/Scrapers/IgdbScraper.cs
@@ -11,9 +11,10 @@ namespace EmulationCoordination.Scrapers.Scrapers
 {
     public class IgdbScraper : BaseScraper
     {
-        private string searchRootUrl = "https://igdbcom-internet-game-database-v1.p.mashape.com/games/";
-        private string searchFormat = "?fields=name%2Cid%2Crelease_dates&limit=10&offset=0&search={0}";
-        private string getDataFormat = "{0}?fields=storyline%2Crating%2Cdevelopers%2Cpublishers%2Cscreenshots%2Cgame_modes&limit=10&offset=0&order=release_dates.date%3Adesc";
+        private string searchRootUrl = "https://igdbcom-internet-game-database-v1.p.mashape.com/";
+        private string searchFormat = "games/?fields=name%2Cid%2Crelease_dates&limit=10&offset=0&search={0}";
+        private string getDataFormat = "games/{0}?fields=storyline%2Crating%2Cdevelopers%2Cpublishers%2Cscreenshots%2Cgame_modes&limit=10&offset=0&order=release_dates.date%3Adesc";
+        private string getCompanyFormat = "companies/{0}?fields=id%2Cname";
         private static string apiKey = "WAV35CMfrrmshHzymVUYTbw3Sz7sp1AjcD0jsnjooQzM79mbIk";
         Dictionary<String, String> headers = new Dictionary<string, string>()
         {
@@ -35,8 +36,36 @@ namespace EmulationCoordination.Scrapers.Scrapers
             if(Results != String.Empty)
             {
                 IgdbData resultData = SerializationUtilities.DeserializeString<List<IgdbData>>(Results, DataFormat.JSON, gameModeConverter)[0];
+                dataToFillOut.Description = String.IsNullOrEmpty(resultData.storyline) ? "No Description Available" : resultData.storyline;
+                dataToFillOut.NumPlayers = resultData.game_modes?.Count() > 0 ? resultData.game_modes[0].NumPlayers : "Unknown";
+                dataToFillOut.Rating = (float)resultData.rating / 100.0f;
+                dataToFillOut.Publisher = RetrieveCompanyInfo(resultData.publishers);
+                dataToFillOut.Developer = RetrieveCompanyInfo(resultData.developers);
             }
             return dataToFillOut;
+        }
+
+        private string RetrieveCompanyInfo(long[] ids)
+        {
+            if(ids.Length > 0)
+            {
+                string finalUrl = String.Format(getCompanyFormat, ids[0]);
+                finalUrl = String.Format("{0}{1}", searchRootUrl, finalUrl);
+                string Results = MakeTextRequest(finalUrl, headers);
+                if (Results != String.Empty)
+                {
+                    IgdbData resultData = SerializationUtilities.DeserializeString<List<IgdbData>>(Results, DataFormat.JSON, gameModeConverter)[0];
+                    return resultData.name;
+                }
+                else
+                {
+                    return "Unknown";
+                }
+            }
+            else
+            {
+                return "Unknown";
+            }
         }
 
         protected override List<RomData> ScraperSpecificSearch(RomData dataToSearchFor)
