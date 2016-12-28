d7ba9accb5c967f67252cc2fbd38388a93fedb25
redsoxfantom <redsoxfantom@gmail.com>
2016-12-28 10:47:57 -0500

    Started work on ControllerManager, a way of mapping controls between emulators in a common fashion

---
+++ EmulationCoordination.Controllers/ControllerManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmulationCoordination.Controllers
+{
+    public class ControllerManager
+    {
+        private static ControllerManager mInstance = null;
+
+        public static ControllerManager Instance
+        {
+            get
+            {
+                if(mInstance == null)
+                {
+                    mInstance = new ControllerManager();
+                }
+                return mInstance;
+            }
+        }
+
+        private ControllerManager()
+        {
+
+        }
+    }
+}
