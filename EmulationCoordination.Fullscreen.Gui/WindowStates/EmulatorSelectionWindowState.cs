442eaf94ee5fac0b5097d6d304f3addeb6d956c5
redsoxfantom <redsoxfantom@gmail.com>
2017-10-01 22:11:25 -0400

    Refactored to move carousel emulator selector to it's own class

--- EmulationCoordination.Fullscreen.Gui/WindowStates/EmulatorSelectionWindowState.cs
+++ EmulationCoordination.Fullscreen.Gui/WindowStates/EmulatorSelectionWindowState.cs
@@ -16,43 +16,14 @@ namespace EmulationCoordination.Fullscreen.Gui.WindowStates
 {
     public class EmulatorSelectionWindowState : IWindowState
     {
-        private List<EmulatorSelection> consoles;
-        private EmulatorSelection selectedConsole;
-        private int selectedConsoleIndex;
         private bool leftRequested = false, rightRequested = false;
+        private Carousel carousel;
         private TextRenderer textRenderer;
-        private TextRenderingOptions opt = new TextRenderingOptions();
 
         public EmulatorSelectionWindowState(TextRenderer textRenderer)
         {
             var inputMgr = InputManager.Instance;
             inputMgr.InputReceived += InputMgr_InputReceived;
-
-            var consolesWithRoms = RomManager.Instance.GetAllRoms();
-            consoles = new List<EmulatorSelection>();
-            foreach(var console in consolesWithRoms.Keys)
-            {
-                EmulatorSelection selection = new EmulatorSelection();
-                selection.console = console;
-                selection.numRoms = consolesWithRoms[console].Count;
-
-                int consoleTexture = GL.GenTexture();
-                GL.BindTexture(TextureTarget.Texture2D, consoleTexture);
-                Bitmap bmp = new Bitmap(console.ConsoleImage);
-                BitmapData data = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height),
-                    ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
-                GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, bmp.Width, data.Height, 0,
-                    OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, data.Scan0);
-                bmp.UnlockBits(data);
-                GL.GenerateMipmap(GenerateMipmapTarget.Texture2D);
-                selection.textureId = consoleTexture;
-
-                consoles.Add(selection);
-            }
-
-            selectedConsole = consoles[0];
-            selectedConsoleIndex = 0;
-
             this.textRenderer = textRenderer;
         }
 
@@ -71,82 +42,49 @@ namespace EmulationCoordination.Fullscreen.Gui.WindowStates
 
         public void Render()
         {
-            GL.PushMatrix();
-            GL.Translate((-3 * selectedConsoleIndex), 0, 0);
-            for(int x = 0; x < consoles.Count; x++)
-            {
-                var console = consoles[x];
-                DrawConsole(x*3, 0, console.textureId);
-            }
-            GL.PopMatrix();
-        }
-
-        private void DrawConsole(double x, double z, int textureId)
-        {
-            GL.BindTexture(TextureTarget.Texture2D, textureId);
-            GL.PushMatrix();
-            GL.Translate(new Vector3((float)x, 0, (float)z));
-            GL.Begin(PrimitiveType.Quads);
-                GL.Color4(Color4.White);
-                GL.TexCoord2(1, 1); GL.Vertex3(-1.0f, -1.0f, 0.0f);
-                GL.TexCoord2(0, 1); GL.Vertex3(1.0f, -1.0f, 0.0f);
-                GL.TexCoord2(0, 0); GL.Vertex3(1.0f, 1.0f, 0.0f);
-                GL.TexCoord2(1, 0); GL.Vertex3(-1.0f, 1.0f, 0.0f);
-            GL.End();
-            GL.PopMatrix();
+            carousel.Render();
         }
 
         public void Update()
         {
-            bool selectedConsoleUpdated = false;
-            if(leftRequested && selectedConsoleIndex < consoles.Count-1)
+            if(leftRequested)
             {
-                selectedConsoleIndex++;
                 leftRequested = false;
-                selectedConsoleUpdated = true;
+                carousel.ChangeSelectedItem(InputType.LEFT);
             }
-            if(rightRequested && selectedConsoleIndex > 0)
+            if(rightRequested)
             {
-                selectedConsoleIndex--;
                 rightRequested = false;
-                selectedConsoleUpdated = true;
-            }
-            selectedConsole = consoles[selectedConsoleIndex];
-            if(selectedConsoleUpdated)
-            {
-                PrintEmulatorData();
+                carousel.ChangeSelectedItem(InputType.RIGHT);
             }
         }
 
         public void Initialize()
         {
-            PrintEmulatorData();
-        }
-
-        private void PrintEmulatorData()
-        {
-            textRenderer.Clear(Color.Blue);
-            opt.Alignment = new TextAlignment(AlignmentHorizontal.ALIGN_CENTER);
-            opt.Location = PointF.Empty;
-            textRenderer.DrawString(selectedConsole.console.FriendlyName, opt);
-            opt.Location = new PointF(0, 100);
-            String numRoms = String.Empty;
-            if (selectedConsole.numRoms > 1)
+            var consolesWithRoms = RomManager.Instance.GetAllRoms();
+            List<CarouselItem> carouselItems = new List<CarouselItem>();
+            foreach (var console in consolesWithRoms.Keys)
             {
-                numRoms = String.Format("{0} Games", selectedConsole.numRoms);
+                CarouselItem consoleItem = new CarouselItem();
+                int numRoms = consolesWithRoms[console].Count;
+                consoleItem.ItemImage = console.ConsoleImage;
+                consoleItem.Tag = console;
+                consoleItem.ItemStrings = new string[2];
+                consoleItem.ItemStrings[0] = console.FriendlyName;
+                string numRomsStrings;
+                if (numRoms > 1)
+                {
+                    numRomsStrings = String.Format("{0} Games", numRoms);
+                }
+                else
+                {
+                    numRomsStrings = String.Format("1 Game");
+                }
+                consoleItem.ItemStrings[1] = numRomsStrings;
+                carouselItems.Add(consoleItem);
             }
-            else
-            {
-                numRoms = String.Format("1 Game");
-            }
-            textRenderer.DrawString(numRoms, opt);
-        }
 
-        private class EmulatorSelection
-        {
-            public EmulatorConsoles console;
-            public int numRoms;
-            public int textureId;
+            carousel = new Carousel(carouselItems, textRenderer);
         }
     }
 }
