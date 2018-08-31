3132fded2826863019bc630af51fce6e8416af07
Tom English <thomas.f.english@lmco.com>
2018-08-31 11:15:57 -0400

    Fixed error in selecting scrapers

--- EmulationCoordination.Gui/Controls/ScraperSelectScraper.Designer.cs
+++ EmulationCoordination.Gui/Controls/ScraperSelectScraper.Designer.cs
@@ -29,47 +29,47 @@
         private void InitializeComponent()
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.RadioButtonGrid = new System.Windows.Forms.TableLayoutPanel();
+            this.cbxScraperSelection = new System.Windows.Forms.ComboBox();
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
             // 
             this.tableLayoutPanel1.ColumnCount = 3;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.Controls.Add(this.RadioButtonGrid, 1, 1);
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 27.27273F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 45.45454F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 27.27273F));
+            this.tableLayoutPanel1.Controls.Add(this.cbxScraperSelection, 1, 1);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 3;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(438, 395);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(328, 321);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
-            // RadioButtonGrid
+            // cbxScraperSelection
             // 
-            this.RadioButtonGrid.ColumnCount = 1;
-            this.RadioButtonGrid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.RadioButtonGrid.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.RadioButtonGrid.Location = new System.Drawing.Point(149, 134);
-            this.RadioButtonGrid.Name = "RadioButtonGrid";
-            this.RadioButtonGrid.RowCount = 1;
-            this.RadioButtonGrid.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.RadioButtonGrid.Size = new System.Drawing.Size(140, 125);
-            this.RadioButtonGrid.TabIndex = 0;
+            this.cbxScraperSelection.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cbxScraperSelection.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxScraperSelection.FormattingEnabled = true;
+            this.cbxScraperSelection.Location = new System.Drawing.Point(92, 109);
+            this.cbxScraperSelection.Name = "cbxScraperSelection";
+            this.cbxScraperSelection.Size = new System.Drawing.Size(143, 24);
+            this.cbxScraperSelection.TabIndex = 0;
             // 
             // ScraperSelectScraper
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.Controls.Add(this.tableLayoutPanel1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "ScraperSelectScraper";
-            this.Size = new System.Drawing.Size(438, 395);
+            this.Size = new System.Drawing.Size(328, 321);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -78,6 +78,6 @@
         #endregion
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.TableLayoutPanel RadioButtonGrid;
+        private System.Windows.Forms.ComboBox cbxScraperSelection;
     }
 }
--- EmulationCoordination.Gui/Controls/ScraperSelectScraper.cs
+++ EmulationCoordination.Gui/Controls/ScraperSelectScraper.cs
@@ -14,40 +14,25 @@ namespace EmulationCoordination.Gui.Controls
     public partial class ScraperSelectScraper : UserControl
     {
         private List<String> scrapers;
-        private List<RadioButton> scraperRadioButtons;
 
         public ScraperSelectScraper()
         {
             InitializeComponent();
-            scraperRadioButtons = new List<RadioButton>();
         }
 
         public void Initialize(List<String> scrapers)
         {
-            this.scrapers = scrapers;
-
-            foreach(var scraper in scrapers)
-            {
-                RadioButton scraperButton = new RadioButton();
-                scraperButton.Text = scraper;
-                scraperButton.Tag = scraper;
-                scraperButton.Dock = DockStyle.Fill;
-
-                RadioButtonGrid.RowCount++;
-                RadioButtonGrid.RowStyles.Add(new RowStyle(SizeType.AutoSize));
-                RadioButtonGrid.Controls.Add(scraperButton, 0, scraperRadioButtons.Count);
-                scraperRadioButtons.Add(scraperButton);
-            }
+            cbxScraperSelection.Items.AddRange(scrapers.ToArray());
         }
 
         public String GetSelectedScraper()
         {
-            var checkedButton = scraperRadioButtons.Where(f => f.Checked);
-            if(checkedButton.Count() < 1)
+            string scraper = (String)cbxScraperSelection.SelectedItem;
+            if(String.IsNullOrEmpty(scraper))
             {
                 return null;
             }
-            return (String)checkedButton.First().Tag;
+            return scraper;
         }
     }
 }
